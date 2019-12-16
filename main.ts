eb62bd34165e6c21b3a67efbc5f9da5177f8ed27
Erica Phelps <erica@blackspork.com>
2019-12-15 18:57:51 -0600

    Cleaned up routing stuff.

--- main.ts
+++ main.ts
@@ -18,6 +18,7 @@ function createWindow(): BrowserWindow {
     y: 0,
     width: size.width,
     height: size.height,
+    title: 'KeyBard',
     webPreferences: {
       nodeIntegration: true,
       allowRunningInsecureContent: (serve) ? true : false,
